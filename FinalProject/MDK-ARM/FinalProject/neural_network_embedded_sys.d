27f9b27492821cd4aa3d8c9793ab9110ff5a8ad5
Jack Marshall <112056604+Jacamarsh@users.noreply.github.com>
2024-04-27 18:40:36 -0700

    Add files via upload

---
+++ FinalProject/MDK-ARM/FinalProject/neural_network_embedded_sys.d
@@ -0,0 +1,9 @@
+finalproject/neural_network_embedded_sys.o: \
+  ..\Drivers\NeuralNetwork\Neural_network_Embedded_sys.c \
+  ..\Drivers\NeuralNetwork\Neural_network_Embedded_sys.h \
+  ..\Drivers\NeuralNetwork\rtwtypes.h \
+  ..\Drivers\NeuralNetwork\rtw_continuous.h \
+  ..\Drivers\NeuralNetwork\rtw_solver.h \
+  C:\Keil_v5\ARM\ARMCLANG\include\string.h \
+  C:\Keil_v5\ARM\ARMCLANG\include\math.h \
+  C:\Keil_v5\ARM\ARMCLANG\include\stddef.h
